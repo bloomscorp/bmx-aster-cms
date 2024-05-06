09bea337e6704351a21ac2e725eff39f73ea4086
Deep-Dey <deepdey20998@gmail.com>
2024-05-06 13:08:41 +0530

    chore: refactoring CSS

--- projects/ngx-aster-cms/src/lib/authentication/login-form/login-form.component.ts
+++ projects/ngx-aster-cms/src/lib/authentication/login-form/login-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'aster-cms-login-form',
   standalone: true,
-  imports: [],
+	imports: [
+		NgOptimizedImage
+	],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
