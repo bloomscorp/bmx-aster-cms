86caa22c5ae06fd4ae7cdaef96871e824a9dfbc5
Deep-Dey <deepdey20998@gmail.com>
2024-08-03 23:21:28 +0530

    release @0.0.30

---
+++ projects/ngx-aster-cms/src/lib/dashboard/model/aster-dashboard-items.spec.ts
@@ -0,0 +1,7 @@
+import { AsterDashboardItems } from './aster-dashboard-items';
+
+describe('AsterDashboardItems', () => {
+  it('should create an instance', () => {
+    expect(new AsterDashboardItems()).toBeTruthy();
+  });
+});
