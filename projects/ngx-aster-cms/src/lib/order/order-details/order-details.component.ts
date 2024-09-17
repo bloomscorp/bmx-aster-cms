609418dc28223978087bd05ee327d57acb1b9566
Deep Dey <deepdey20998@gmail.com>
2024-09-17 11:44:41 +0530

    feat: add order-details ui

---
+++ projects/ngx-aster-cms/src/lib/order/order-details/order-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'aster-cms-order-details',
+  standalone: true,
+	imports: [
+		MatIcon
+	],
+  templateUrl: './order-details.component.html',
+  styleUrl: './order-details.component.scss'
+})
+export class OrderDetailsComponent {
+
+}
