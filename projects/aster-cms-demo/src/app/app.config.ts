f269dc59ef65c4c6de32a5883c7455eafb5dca1e
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-09-26 00:25:04 +0530

    chore: fix imports

--- projects/aster-cms-demo/src/app/app.config.ts
+++ projects/aster-cms-demo/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {AsterProductListingService} from "../../../ngx-aster-cms/src/lib/product/service/aster-product-listing.service";
+import {AsterProductListingService} from "../../../ngx-aster-cms/src/lib/product/service/aster-product-listing-service";
 import {ProductListingService} from "./product/service/product-listing.service";
 
 export const appConfig: ApplicationConfig = {
--- projects/aster-cms-demo/src/app/product/service/product-listing.service.ts
+++ projects/aster-cms-demo/src/app/product/service/product-listing.service.ts
@@ -1,12 +1,11 @@
 import {Injectable} from '@angular/core';
-import {AbstractProductListingService} from "../../../../../ngx-aster-cms/src/lib/product/service/abstract-product-listing.service";
-import {AsterProductListingService} from "../../../../../ngx-aster-cms/src/lib/product/service/aster-product-listing.service";
+import {AsterProductListingService} from "../../../../../ngx-aster-cms/src/lib/product/service/aster-product-listing-service";
 import {ProductListingScreenData} from "../../../../../ngx-aster-cms/src/lib/product/interface/screen/product-listing-screen-data";
 
 @Injectable({
 	providedIn: 'root'
 })
-export class ProductListingService extends AsterProductListingService{
+export class ProductListingService extends AsterProductListingService {
 
 	constructor() {
 		super();
