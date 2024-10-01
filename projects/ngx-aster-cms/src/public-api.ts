f22de2e943eaaa2b993c26871f2d76081cae821b
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-10-01 15:36:21 +0530

    chore: release 0.0.37

--- projects/ngx-aster-cms/src/public-api.ts
+++ projects/ngx-aster-cms/src/public-api.ts
@@ -6,6 +6,18 @@ export * from './lib/route/model/default-routes';
 
 export * from './lib/authentication/login/login.component';
 
+/* security */
+export * from './lib/authentication/jwt.service';
+
+/* component library: aster table */
+export * from './lib/component-library/aster-table/aster-table/aster-table.component';
+export * from './lib/component-library/aster-table/interface/cell-action';
+export * from './lib/component-library/aster-table/interface/cell-default-actions';
+export * from './lib/component-library/aster-table/interface/table-column';
+export * from './lib/component-library/aster-table/interface/table-column-data';
+export * from './lib/component-library/aster-table/interface/table-data';
+export * from './lib/component-library/aster-table/interface/table-header';
+
 /* navigation */
 export * from './lib/navigation/navigation/navigation.component';
 export * from './lib/navigation/service/aster-navigation-mapper.service';
@@ -24,9 +36,6 @@ export * from './lib/dashboard/model/aster-dashboard-items';
 export * from './lib/product/service/aster-product-service';
 export * from './lib/product/service/aster-product-listing-service';
 export * from './lib/product/service/aster-product-details-service';
-
-/* security */
-export * from './lib/authentication/jwt.service';
 
 /* transmission */
 export * from './lib/raintree/transmission/crud-transmission-service';
