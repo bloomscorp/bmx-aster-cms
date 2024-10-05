071758ae2719a4cb3d700b23c67a337346e943d2
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-10-05 18:43:14 +0530

    feat: update OrderListingComponent:

--- projects/ngx-aster-cms/src/lib/order/order-listing/order-listing.component.ts
+++ projects/ngx-aster-cms/src/lib/order/order-listing/order-listing.component.ts
@@ -1,30 +1,33 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {HeaderSectionComponent} from "../../component-library/header/header-section/header-section.component";
 import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
 import {OrderItem} from "../interface/order-item";
 import {OrderItemList} from "../model/order-item-list";
 import {DatePipe} from "@angular/common";
+import {OrderListingScreenData} from "../interface/screen/order-listing-screen-data";
+import {AsterOrderListingService} from "../service/aster-order-listing-service";
+import {AsterTableComponent} from "../../component-library/aster-table/aster-table/aster-table.component";
 
 @Component({
 	selector: 'aster-cms-order-listing',
 	standalone: true,
 	imports: [
-		HeaderSectionComponent,
-		MatIcon,
-		RouterLink,
-		DatePipe
+		AsterTableComponent,
+		HeaderSectionComponent
 	],
 	templateUrl: './order-listing.component.html',
 	styleUrl: './order-listing.component.scss'
 })
-export class OrderListingComponent {
+export class OrderListingComponent<O> implements OnInit {
 
-	@Input() heading: string = 'Manage Orders';
-	@Input() subHeading: string = 'View and manage your orders here';
+	public screen: OrderListingScreenData = {} as OrderListingScreenData;
 
-	public orderItems: OrderItem[] = OrderItemList.orderItems;
+	constructor(public _: AsterOrderListingService<O>) {
+	}
 
-	constructor() {
+	public ngOnInit() {
+		this.screen = this._.screen;
+		this._.fetchOrderList();
 	}
 }
