60ebb14d3f526db24d934726322d6cce3aded4a7
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-11-25 18:02:50 +0530

    chore: test ngModel with Aster form

--- projects/ngx-aster-cms/src/lib/component-library/aster-form/aster-form-input-text/aster-form-input-text.component.ts
+++ projects/ngx-aster-cms/src/lib/component-library/aster-form/aster-form-input-text/aster-form-input-text.component.ts
@@ -1,14 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {AsterFormInput} from "../interface/aster-form-input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
 	selector: 'aster-form-input-text',
 	standalone: true,
-	imports: [],
+	imports: [
+		FormsModule
+	],
 	templateUrl: './aster-form-input-text.component.html',
 	styleUrl: './aster-form-input-text.component.scss'
 })
 export class AsterFormInputTextComponent<T> {
 
 	@Input() data: AsterFormInput<T, string> = {} as AsterFormInput<T, string>;
+
+	public keyPress(event: any): void {
+		console.log(`v => ${event.target.value}`);
+		console.log(`d => ${this.data.defaultValue}`);
+	}
 }
